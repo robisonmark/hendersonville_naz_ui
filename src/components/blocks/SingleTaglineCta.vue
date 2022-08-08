27082fc0a7481fb50333570c913074593a29324c
Mark Robison <markrobison630@gmail.com>
2022-08-08 00:12:33 -0500

    adding components

---
+++ src/components/blocks/SingleTaglineCta.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :class="components['GhostTwoLineButton'].componentContent.overlap ? 'circle--overlap__centered' : null">
+    <template v-html="components['WysiwygEditor'].componentContent" :class="components['WysiwygEditor'].componentClass" v-hoist></template>
+    <GhostTwoLine v-model="components['GhostTwoLineButton'].componentContent"></GhostTwoLine>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GhostTwoLine from "../buttons/GhostTwoLine.vue";
+
+// types 
+interface ComponentObject {
+    componentName: string;
+    componentContent: string;
+    componentClass?: string;
+            
+}
+
+const props = defineProps({
+  modelValue: Object,
+  componentClass: String,
+});
+
+const components = props.modelValue?.reduce((acc: ComponentObject, curr: ComponentObject) => {
+    const mv: ComponentObject = {...acc};
+    mv[curr['componentName']] = curr
+
+    return mv;
+}, {})
+</script>
+
+<style lang="scss" scoped>
+.circle--overlap__centered {
+  position: absolute;
+  right: calc((45rem - 10rem) / (2 * 3.14));
+}
+</style>
