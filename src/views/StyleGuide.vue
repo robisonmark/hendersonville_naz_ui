872c01b09e4ddc07547f7d771fb1c6ac4c14a540
Mark Robison <ballaboy@gmail.com>
2022-12-18 19:33:44 +0000

    adding in base to style guide

---
+++ src/views/StyleGuide.vue
@@ -0,0 +1,15 @@
+<script lang="javascript">
+  import FullHero from "../components/heros/FullWidthHero.vue";
+  import ContentLeft from "../components/blocks/ContentLeft.vue";
+  import FullWidthFeature from "@/components/blocks/FullWidthFeature.vue";
+  import CardRow from "@/components/blocks/CardRow.vue";
+</script>
+
+<template>
+  <div class="container">
+    <full-hero></full-hero>
+    <content-left></content-left>
+    <full-width-feature></full-width-feature>
+    <content-left></content-left>
+  </div>
+</template>
