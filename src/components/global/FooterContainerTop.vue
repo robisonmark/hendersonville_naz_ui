207d31fa00cd149592c7d72d245005f19994a0c7
Mark Robison <ballaboy@gmail.com>
2022-12-28 15:29:34 -0600

    adding top curve to footer

---
+++ src/components/global/FooterContainerTop.vue
@@ -0,0 +1,38 @@
+<template>
+  <svg
+    viewBox="0 0 1920 85"
+    version="1.1"
+    id="footerContainerTop"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:svg="http://www.w3.org/2000/svg"
+  >
+    <defs id="defs9081" />
+    <g id="layer1" transform="translate(-0.89648438,-147.5)">
+      <path
+        id="ellipse9083"
+        style="fill: #54a3db; fill-opacity: 1"
+        d="M 960,147.5 A 1768,393 0 0 0 0.89648438,210.98828 V 232.5 H 1920.8965 V 210.7168 A 1768,393 0 0 0 960,147.5 Z"
+      />
+    </g>
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FooterContainerTop",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped lang="scss">
+#footerContainerTop {
+  width: 100%;
+  position: relative;
+  margin-block-end: -3px;
+  z-index: -1;
+}
+</style>
